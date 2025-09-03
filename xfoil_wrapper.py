2e03417fa7daa191a15557d5a89f56dbdf0b8df0
Joao Zucchi <joaoherciliozucchi@hotmail.com>
2025-09-03 10:54:36 -0300

    Added the XFoil ASEQ command

--- xfoil_wrapper.py
+++ xfoil_wrapper.py
@@ -1,46 +1,64 @@
-from xfoil import XFoil
-from xfoil.model import Airfoil
 import numpy as np
 import matplotlib.pyplot as plt
+import subprocess
+import tempfile
+import os
 
-def load_airfoil_from_file(file_path):
-    x_coords = []
-    y_coords = []
-    
-    with open(file_path, 'r') as file:
-
-        next(file)
+class XFoil:
+    def __init__(self, airfoil_file, reynolds = 1e5, mach=0.0):
+        self.xfoil_path = 'xfoil'
+        self.airfoil_file = airfoil_file
+        self.reynolds = reynolds
+        self.mach = mach
         
-        for line in file:
-            x, y = map(float, line.strip().split())
-            x_coords.append(x)
-            y_coords.append(y)
+    def aseq(self, alpha_start, alpha_end, alpha_step):
+        commands = [
+            f'LOAD {self.airfoil_file}',
+            'PPAR',
+            'N 160',
+            '',
+            '',
+            'OPER',
+            #f'RE {self.reynolds}'
+            'PACC',
+            'polar.txt',
+            '',
+            f'ASEQ {alpha_start} {alpha_end} {alpha_step}',
+            'QUIT'
+        ]
+        stdout, stderr = self._run_commands(commands)
+        return self._parse_polar_file('polar.txt')
     
-    x_coords = np.array(x_coords)
-    y_coords = np.array(y_coords)
-    
-    return x_coords, y_coords
+    def _run_commands(self, commands):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            input_file = os.path.join(tmpdir, 'input.in')
+            with open(input_file, 'w') as f:
+                f.write('\n'.join(commands))
+            
+            result = subprocess.run(
+                [self.xfoil_path],
+                stdin=open(input_file, 'r'),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+            return result.stdout, result.stderr     
+           
+    def _parse_polar_file(self, filename):
+        data = np.loadtxt(filename, skiprows=12)
+        return {
+            'alpha': data[:, 0],
+            'CL': data[:, 1],
+            'CD': data[:, 2],
+            'CDp': data[:, 3],
+            'CM': data[:, 4],
+        }
 
-x, y = load_airfoil_from_file("airfoils/E63.dat")
+xfoil = XFoil('airfoils/NACA0012.dat')
+results = xfoil.aseq(alpha_start=-12, alpha_end=12, alpha_step=0.5)
 
-xf = XFoil()
-xf.airfoil = Airfoil(x,y)
-xf.repanel()
-plt.plot(xf.airfoil.x, xf.airfoil.y)
-plt.axis('equal')
+plt.plot(results['alpha'], results['CL'])
+plt.xlabel('Alpha')
+plt.ylabel('CL')
 plt.grid()
 plt.show()
-
-xf.Re = 1e6
-xf.max_iter = 40
-a, cl, cd, cm, cp = xf.aseq(-8, 8, 0.5)
-
-#plt.plot(a, cl)
-#plt.show()
-
-# from xfoil.test import naca0012
-# xf.airfoil = naca0012
-# plt.plot(xf.airfoil.x, xf.airfoil.y)
-# plt.axis('equal')
-# plt.grid()
-# plt.show()
